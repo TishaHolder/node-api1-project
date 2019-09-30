31c955421c21b35bd5a3b6e8a9ca2bd8207ccda6
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-09-30 14:49:03 -0400

    completed delete request

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ const express = require ('express');
 //creates an express application using the express module
 const server = express();
 
+//middleware
+//need this for post and put - teaches express how to read json from the request body
+server.use(express.json()); 
+
 //import the db.js file to get access to the DB 
 const DB = require('./data/db.js');
 
 //configures server for every get request to /
-server.get('/', (res, req) => {
+//request comes first followed by the response
+server.get('/', (req, res) => {
 
     res.send('Welcome to the Users Database');
 
 });
 
 //configures server for every get request to /users
-server.get('/users', (res, req) => {
+server.get('/users', (req, res) => {
 
     //calling find returns a promise that resolves to an array of all the users contained in the database.
     DB.find()
-    .then(db => {
+    .then(users => {
         //.json will convert the data passed to JSON
         //also tells the client we're sending JSON through the HTTP header
-        res.status(200).json(db);
+        res.status(200).json(users);
 
     })
     .catch(error => {
@@ -36,12 +41,15 @@ server.get('/users', (res, req) => {
 
 });
 
-server.get('/users/:id', (res, req) => {
+server.get('/users/:id', (req, res) => {
+
+    //params is an object with all the url parameters
+    const userId = req.params.id;
 
     //findById() expects an id as it's only parameter and returns the user corresponding to the id provided
-    DB.findById(id)
-    .then(db => {
-        res.status(200).json(db);
+    DB.findById(userId)
+    .then(users => {
+        res.status(200).json(users);
 
     })
     .catch(error => {
@@ -51,27 +59,45 @@ server.get('/users/:id', (res, req) => {
 });
 
 //creates a user using the information sent inside the request body.
-server.post('/users', (res, req) => {
+server.post('/users', (req, res) => {
 
-    const dbInformation = req.body;
+    const dbInformation = req.body;    
 
-    
-
-    DB.insert(dbInformation)
-    
-    .then(db => {
+    DB.insert(dbInformation)    
+    .then(users => {
         if(dbInformation.name === "" || dbInformation.bio === ""){
 
-            res.status(400).json({message: 'Please provide name and bio for the user.'});
-    
+            res.status(400).json({message: 'Please provide name and bio for the user.'});    
         }
         else {
 
-            res.status(201).json(db);
+            res.status(201).json(dbInformation);
         }
     })
     .catch(error => {
+
         res.status(500).json({message: 'There was an error while saving the user to the database.'});
+    })
+
+});
+
+server.delete('/users/:id', (req, res) => {
+
+    const userId = req.params.id;
+
+    DB.remove(userId)
+    .then(users => {
+        if(userId) {
+            res.status(200).json(users);
+        }
+        else {
+
+            res.status(404).json({message: 'The user with the specified ID does not exist.'});
+        }        
+
+    })
+    .catch(error => {
+        res.status(500).json({message: 'The user could not be removed.'});
     })
 
 });
